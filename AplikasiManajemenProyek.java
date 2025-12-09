5351372de5e589d4a5274d7555dcb37910d5e7a6
Nelsen Reynardo <nelsenreynardo04@gmail.com>
2025-12-09 14:50:37 +0700

    Membuat Fitur Registrasi

--- AplikasiManajemenProyek.java
+++ AplikasiManajemenProyek.java
@@ -1,15 +1,37 @@
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class AplikasiManajemenProyek{
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+
+    public static void login(String usn, String pass){
         
+    }
+
+    public static void register(String usn, String pass){
+        
+        try {
+            FileWriter fw = new FileWriter("UserData.txt");
+            fw.write(usn + ":" + pass);
+            fw.close();
+            System.out.println("Akun Berhasil Dibuat!");
+        } catch (IOException e) {
+            System.out.println("Terjadi Error Saat Penulisan File!");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        //Inisialisasi 
+        Scanner input = new Scanner(System.in);
+
         
 
         //Onboarding Page (Halaman Pembuka)
 
         String asciiColorMerah = "\u001B[31m";
         String asciiColorHijau = "\u001B[32m";
+        String asciiColorBiru = "\u001B[34m";
         String asciiColorReset = "\u001B[0m";
         System.out.println(asciiColorHijau + "============================================================================" + asciiColorReset);
         System.out.println(asciiColorMerah + "______          _           _    ___  ___" + asciiColorReset);
@@ -22,7 +44,37 @@ public class AplikasiManajemenProyek{
         System.out.println(asciiColorMerah + "             |__/                                          |___/" + asciiColorReset);
         System.out.println(asciiColorHijau + "============================================================================" + asciiColorReset);
 
-        System.out.println("Selamat Datang di Aplikasi Manajemen Proyek! ");
+        System.out.println(asciiColorBiru + "               Selamat Datang di Aplikasi Manajemen Proyek!" + asciiColorReset);
+        //Select Menu
+        System.out.println("[1] Login");
+        System.out.println("[2] Register");
+        System.out.println("[3] About");
+        System.out.println("[4] Exit");
+        System.out.println(" ");
+        System.out.print("Masukkan Pilihan: ");
+        int pilihanMenu = input.nextInt();
+        if(pilihanMenu == 1){
+            System.out.print("-> Masukkan Username: ");
+            String username = input.next();
+            System.out.print("-> Masukkan Password");
+            String password = input.next();
+            login(username, password);
+        }else if(pilihanMenu == 2){
+            System.out.print("-> Buat Username: ");
+            String regUsername = input.next();
+            System.out.print("-> Buat Password untuk akun " + regUsername + ": ");
+            String regPassword = input.next();
+            register(regUsername, regPassword);
+        }else if(pilihanMenu == 3){
+
+        }else if(pilihanMenu == 4){
+            System.out.println("-> Aplikasi akan ditutup!");
+            System.exit(0);
+        }else{
+            System.out.println("-> Input Tidak Valid!");
+        }
+
+
     }
 }
 
